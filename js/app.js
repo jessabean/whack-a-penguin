62309001aed5452e5c186bd94f6964732abcae32
jessabean <jessica.avison@gmail.com>
2016-08-01 14:57:42 -0400

    Combine hiding and shuffling, add a start function

--- js/app.js
+++ js/app.js
@@ -1,31 +1,32 @@
 var hideout = document.getElementById('yeti-hideout');
 
-function Game() {
+var Game = function() {
   this.MOUNDS = 9;
   this.penguins = [];
   this.yeti = Math.floor(Math.random() * this.MOUNDS);
-}
-
-Game.prototype.hidePenguins = function() {
-  for(var i=0; i<this.MOUNDS - 1; i++) {
-    this.penguins.push('penguin'+i);
-  }
-
-  this.shufflePenguins(this.penguins);
 };
 
-Game.prototype.shufflePenguins = function(array) {
+Game.prototype.start = function() {
+  this.hidePenguins(this.penguins);
+}
+
+Game.prototype.hidePenguins = function(penguins) {
+  for(var i=0; i<this.MOUNDS - 1; i++) {
+    penguins.push('penguin'+i);
+  }
+
   var i = 0,
       j = 0,
       temp = null;
 
-
-  for (i = array.length - 1; i > 0; i -= 1) {
+  for (i = penguins.length - 1; i > 0; i -= 1) {
     j = Math.floor(Math.random() * (i + 1));
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+    temp = penguins[i];
+    penguins[i] = penguins[j];
+    penguins[j] = temp;
   }
 
-  return this.penguins = array;
+  return penguins;
 };
+
+module.exports = Game;
