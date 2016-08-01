803ad505062c658c5de1d9e42375e902ec799539
jessabean <jessica.avison@gmail.com>
2016-08-01 14:36:48 -0400

    Set up penguins list and shuffle the penguins

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
+var hideout = document.getElementById('yeti-hideout');
 
+function Game() {
+  this.MOUNDS = 9;
+  this.penguins = [];
+  this.yeti = Math.floor(Math.random() * this.MOUNDS);
+}
+
+Game.prototype.hidePenguins = function() {
+  for(var i=0; i<this.MOUNDS - 1; i++) {
+    this.penguins.push('penguin'+i);
+  }
+
+  this.shufflePenguins(this.penguins);
+};
+
+Game.prototype.shufflePenguins = function(array) {
+  var i = 0,
+      j = 0,
+      temp = null;
+
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return this.penguins = array;
+};
